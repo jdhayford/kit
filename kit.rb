dc71b6f322717349469bb3b441f099bffa1e9530
jdhayford <jdhayford@gmail.com>
2022-01-04 16:13:08 -0500

    Brew formula update for kit version v0.0.4

--- kit.rb
+++ kit.rb
@@ -11,23 +11,23 @@ class Kit < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/jdhayford/kit/releases/download/v0.0.3/kit_0.0.3_Darwin_x86_64.tar.gz"
-      sha256 "60808411a758d150cab72a5eefc6b9199a1d3982a9d9d29aec5f9122c0584153"
+      url "https://github.com/jdhayford/kit/releases/download/v0.0.4/kit_0.0.4_Darwin_x86_64.tar.gz"
+      sha256 "cbbba2657f6f6490994fbba8cca132b7be67d1c8338d8e1faad246a25f498f08"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/jdhayford/kit/releases/download/v0.0.3/kit_0.0.3_Darwin_arm64.tar.gz"
-      sha256 "821e575f0f2431873f4c696e99651db2f93f812330c5b70197f401731e25b672"
+      url "https://github.com/jdhayford/kit/releases/download/v0.0.4/kit_0.0.4_Darwin_arm64.tar.gz"
+      sha256 "70154004e0216282ff5d7180cac4fbaf782739f8b6926dc9130354dbf4fcfab6"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/jdhayford/kit/releases/download/v0.0.3/kit_0.0.3_Linux_x86_64.tar.gz"
-      sha256 "59b4b5dbe2f1ecf80762741ed90e801a9d84c17a20fa0731304087f10debd8ed"
+      url "https://github.com/jdhayford/kit/releases/download/v0.0.4/kit_0.0.4_Linux_x86_64.tar.gz"
+      sha256 "4d5f39fdee418e67fd7be0ae4228325d5d5d93487b31df3d1727112b12979e51"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jdhayford/kit/releases/download/v0.0.3/kit_0.0.3_Linux_arm64.tar.gz"
-      sha256 "536058ee5f35022b7971f41b295c413363530f0e87cd3b6c9f9f8d621640ff55"
+      url "https://github.com/jdhayford/kit/releases/download/v0.0.4/kit_0.0.4_Linux_arm64.tar.gz"
+      sha256 "33504b1900d29314cde14dc40a7f43251136e1399a222edc43a7e62b1f899b7a"
     end
   end
 
